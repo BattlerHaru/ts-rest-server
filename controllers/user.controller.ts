f984f5a47e21137a691404e4fa1c4b72ad9c54e6
BattlerHaru <kronk.sdance@gmail.com>
2023-05-18 23:20:34 -0600

    se agregaron las funciones del crud

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,44 +1,104 @@
 import { Request, Response } from 'express';
+import User from '../models/user.model';
 
-export const getUsers = (req: Request, res: Response) => {
+export const getUsers = async (req: Request, res: Response) => {
+    const users = await User.findAll();
     res.json({
-        msg: "getUsers"
+        msg: "getUsers",
+        users
     })
 }
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async (req: Request, res: Response) => {
     const { id } = req.params;
-    res.json({
-        msg: "getUser",
-        id
-    })
+    const user = await User.findByPk(id);
+    if (user) {
+        res.json({
+            msg: "getUser",
+            user
+        })
+    } else {
+        res.status(404).json({
+            msg: "getUser",
+            error: "Usuario no encontrado"
+        })
+    }
 }
 
-export const postUser = (req: Request, res: Response) => {
+export const postUser = async (req: Request, res: Response) => {
     const { body } = req;
-    console.log(body);
+    try {
 
-    res.json({
-        msg: "postUser",
-        body
-    })
+        const emailExists = await User.findOne({
+            where: {
+                email: body.email
+            }
+        });
+
+        if (emailExists) {
+            return res.status(400).json({
+                msg: `Ya existe un usuario con el email: ${body.email}`
+            })
+        }
+
+        // const user = User(body);
+        const user = await User.create(body);
+        await user.save();
+        res.status(201).json({
+            msg: "postUser",
+            user
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "Ha ocurrido un error",
+            error
+        })
+    }
 }
-export const putUser = (req: Request, res: Response) => {
+export const putUser = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { body } = req;
+    try {
 
-    res.json({
-        msg: "putUser",
-        id,
-        body
-    })
+
+        const user = await User.findByPk(id);
+        if (!user) {
+            return res.status(400).json({
+                msg: `No existe un usuario con ese ID: ${id}`
+            })
+        }
+
+        await user.update(body);
+        res.status(200).json({
+            msg: "putUser",
+            user
+        })
+    } catch (error) {
+        res.status(500).json({
+            msg: "Ha ocurrido un error",
+            error
+        })
+    }
 }
 
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
     const { id } = req.params;
 
-    res.json({
-        msg: "deleteUser",
-        id,
+    const user = await User.findByPk(id);
+    if (!user) {
+        return res.status(400).json({
+            msg: `No existe un usuario con ese ID: ${id}`
+        })
+    }
+
+    //Eliminación física
+    // await user.destroy();
+
+    //Eliminación lógica
+    await user.update({
+        status: false
     })
+
+    res.json(user)
+
 }
